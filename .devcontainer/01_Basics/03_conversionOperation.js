db076982c4b3d3c5811a82b4bfb93480feed04f6
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-14 18:37:04 +0000

    conversion

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score= "33 abc"// string as in "...", if not within " " , then a number.
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);//number
+console.log(ValueInNumber); // NaN
+
+
+//"33" => 33
+//"33abc" => NaN ( but type is number)
+// true=> 1; false=>0
+
+let isLoggedIn = 1
+
+let  booleanIsLoggesdIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggesdIn) // true.
+
+//1 => true; 0=>false;
+//""=> false
+//"Debu"=> true
+
+let strNumber=33
+let stringNumber = String(strNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
