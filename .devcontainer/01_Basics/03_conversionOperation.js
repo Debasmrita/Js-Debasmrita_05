37678892f44c73655f21185c14e7416eff80bdcc
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-15 06:12:21 +0000

    conversionOpn Done

--- .devcontainer/01_Basics/03_conversionOperation.js
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -26,3 +26,39 @@ let strNumber=33
 let stringNumber = String(strNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber))
+
+console.log("************Operations**************")
+
+/* console.log(2+2);//4
+console.log(2*2);//4
+console.log(2**2);//4
+console.log(9/3);//3
+console.log(3%2);//1
+*/
+
+//String concatenation
+str1="helllo";
+str2=" Debu";
+str3=str1 + str2;
+console.log(str3);//helllo Debu
+
+
+//Confusion arrises when...
+// console.log("1" + 2);//12 (reading as String)
+// console.log(1 + "2");//12 (reading as String)
+// console.log("1" + "2");//12 (reading as String)
+// console.log("1" + 2 + 2);//122 (reading as String)
+// console.log(1 + 2 + "2");//32(int this case the first two are taken as numbers , and then gets concatenated with string "2")
+
+
+//precedence ----> prefix and postfix.
+let gameCounter=100;
+// gameCounter++;//101
+++gameCounter;//101
+console.log(gameCounter);
+
+// chatgpt link for TypeConversion: 
+//https://chatgpt.com/share/684e4fe6-a2ec-8013-a1c1-5de4c0680e44
+
+//mdn link for prefix and postfix:-   
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
