5d03937959cd758a948dc8e1637f18e240a50d4a
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-16 17:41:53 +0000

    strings done

--- .devcontainer/01_Basics/05_strings.js
+++ .devcontainer/01_Basics/05_strings.js
@@ -5,19 +5,29 @@ const repoCount= 50;
 
 console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Debu_js')
+const gameName = new String('hite-sh-hc')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName[0]);//h
+console.log(gameName.__proto__);//{}
 
 
-console.log(gameName.length);
-console.log(String(gameName).toUpperCase());
-console.log(String(gameName).charAt(2));
-console.log(String(gameName).indexOf('b'));
+console.log(gameName.length);//8
+// console.log(String(gameName).toUpperCase());
+// console.log(String(gameName).charAt(2));
+// console.log(String(gameName).indexOf('b'));
 
 const newString = gameName.substring(0,3);
-console.log(newString)
+console.log(newString)//hit
 
 const anotherString = gameName.slice(-8,3);
-console.log(anotherString)
+console.log(anotherString)//hit
+
+const StringOne = "        Debu        ";
+console.log(StringOne);//             Debu             
+console.log(StringOne.trim());//Debu
+
+const url = "https://debu.com/debuBanerjee%202005"
+
+console.log(url.replace('%20', '-'));
+
+console.log(gameName.split('-'))
---
+++ .devcontainer/01_Basics/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(gameName.length);
+// console.log(String(gameName).toUpperCase());
+// console.log(String(gameName).charAt(2));
+// console.log(String(gameName).indexOf('b'));
