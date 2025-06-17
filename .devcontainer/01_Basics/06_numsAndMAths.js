d486c6a29e1a7aa5952e7a0e46b5d4ca585b07aa
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-17 11:21:22 +0000

    comit done

--- .devcontainer/01_Basics/06_numsAndMAths.js
+++ .devcontainer/01_Basics/06_numsAndMAths.js
@@ -1,23 +1,23 @@
-// const score = 400;
-// console.log(score);//400
+const score = 400;
+console.log(score);//400
 
-// const balance = new Number(100);
-// console.log(balance);//[Number: 100]
-
-
-// console.log(balance.toString());//100
-// console.log(typeof(balance.toString()));//string
-// console.log((balance.toString()).length);//3
-
-// console.log(balance.toFixed(2));//100.00
-
-// const otherNumber = 23.66698;
-// console.log(otherNumber.toPrecision(2));//24
-// console.log(otherNumber.toPrecision(3));//23.7
+const balance = new Number(100);
+console.log(balance);//[Number: 100]
 
 
-// const hundreds = 1000000;
-// console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+console.log(balance.toString());//100
+console.log(typeof(balance.toString()));//string
+console.log((balance.toString()).length);//3
+
+console.log(balance.toFixed(2));//100.00
+
+const otherNumber = 23.66698;
+console.log(otherNumber.toPrecision(2));//24
+console.log(otherNumber.toPrecision(3));//23.7
+
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));//10,00,000
 
 //++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++
 
