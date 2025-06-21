62b5712cd80b30296ab8c385433a20728f8a25fb
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-21 08:31:46 +0000

    arrays_01

--- .devcontainer/02_Basics/01_arrays.js
+++ .devcontainer/02_Basics/01_arrays.js
@@ -5,39 +5,39 @@ const myArr=[0, 1,2,3,4,5];
 const myHeroes=["Captain America", "iron-man"];
 
 const myArr2=new Array(1,2,3,4)
-//console.log(myArr[1]);//1
+console.log(myArr[1]);//1
 
 //Array Methods
 
-// myArr.push(6);/*[
-//   0, 1, 2, 3,
-//   4, 5, 6
-// ]*/
-// myArr.push(7);/*[
-//   0, 1, 2, 3,
-//   4, 5, 6, 7
-// ]*/ 
+myArr.push(6);/*[
+  0, 1, 2, 3,
+  4, 5, 6
+]*/
+myArr.push(7);/*[
+  0, 1, 2, 3,
+  4, 5, 6, 7
+]*/ 
 
-// myArr.pop();/*[
-//   0, 1, 2, 3,
-//   4, 5, 6
-// ]*/
+myArr.pop();/*[
+  0, 1, 2, 3,
+  4, 5, 6
+]*/
 
-// myArr.unshift(9);/*[
-//   9, 0, 1, 2,
-//   3, 4, 5
-// ]*/
+myArr.unshift(9);/*[
+  9, 0, 1, 2,
+  3, 4, 5
+]*/
 
-// myArr.shift();//[ 0, 1, 2, 3, 4, 5 ]
-
-
-//console.log(myArr.includes(3));// returns value in true or false.
+myArr.shift();//[ 0, 1, 2, 3, 4, 5 ]
 
 
+console.log(myArr.includes(3));// returns value in true or false.
 
-//const newArr= myArr.join()
-// console.log(newArr);//0,1,2,3,4,5{changes the array to string seperated by comma(,)}
-// console.log(myArr);//[ 0, 1, 2, 3, 4, 5 ]
+
+
+const newArr= myArr.join()
+console.log(newArr);//0,1,2,3,4,5{changes the array to string seperated by comma(,)}
+console.log(myArr);//[ 0, 1, 2, 3, 4, 5 ]
 
 
 //Slice &  Splice
