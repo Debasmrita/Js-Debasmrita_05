9d94ce9e9470f6d3d039300eb8fa5e04c7e4f1f0
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-22 04:49:21 +0000

    objects

--- .devcontainer/02_Basics/04_objects.js
+++ .devcontainer/02_Basics/04_objects.js
@@ -1,61 +1,61 @@
-// const tinderUser = new Object()// singleton object
-// //const tinderUser ={};// non-singelton object
+const tinderUser = new Object()// singleton object
+//const tinderUser ={};// non-singelton object
 
 
-// tinderUser.id= "123abc";
-// tinderUser.name= "Debu";
-// tinderUser.isLoggedIn = false;
+tinderUser.id= "123abc";
+tinderUser.name= "Debu";
+tinderUser.isLoggedIn = false;
 
-// //console.log(tinderUser);
+//console.log(tinderUser);
 
 
-// const regularUser ={
-//     email: "debu@gmail.com",
-//     fullNmae: {
-//         userfullname: {
-//             firdtName: "Debu",
-//             lastname:"Banerjee"
-//         }
-//     }
-// }
+const regularUser ={
+    email: "debu@gmail.com",
+    fullNmae: {
+        userfullname: {
+            firdtName: "Debu",
+            lastname:"Banerjee"
+        }
+    }
+}
 
-// //console.log(regularUser.fullNmae.userfullname.firdtName);
+//console.log(regularUser.fullNmae.userfullname.firdtName);
 
-// const obj1= {
-//     1: "a",
-//     2: "b",
-// }
-// const obj2= {
-//     3: "c",
-//     4: "d",
-// }
+const obj1= {
+    1: "a",
+    2: "b",
+}
+const obj2= {
+    3: "c",
+    4: "d",
+}
 
-// //const obj3 = Object.assign({}, obj1 , obj2); // important!
+//const obj3 = Object.assign({}, obj1 , obj2); // important!
 
-// const obj3= {...obj1,...obj2}// use spread and chill!
-// console.log(obj3);
+const obj3= {...obj1,...obj2}// use spread and chill!
+console.log(obj3);
 
-// const user= [
-//     {
-//         id: 1,
-//         email: "d@gmail.com"
-//     },
-//     {
+const user= [
+    {
+        id: 1,
+        email: "d@gmail.com"
+    },
+    {
 
-//     },
-//     {
+    },
+    {
 
-//     }
-// ]
+    }
+]
 
-// user[1].email
-// console.log(tinderUser);
+user[1].email
+console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 // de-Structuring
@@ -70,7 +70,7 @@ const {courseInztructor}= course
 console.log(courseInztructor);//Debu-DiDi
 
 //API---> Apna Kaam, dalo kisi aur ki Upar!!
-//Json format---
+//Json format--->
 // {
 //     "name": "DEbu",
 //     "coursename": "js in hindi",
