efa3577681b29433ce6e44f3dba29d03a1327624
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-22 02:50:46 +0000

    objects done

--- .devcontainer/02_Basics/tempCodeRunnerFile.js
+++ .devcontainer/02_Basics/tempCodeRunnerFile.js
@@ -1,3 +1,5 @@
-console.log(newArr);//0,1,2,3,4,5{changes the array to string seperated by comma(,)}
-// console.log(myArr);//[ 0, 1, 2, 3, 4, 5 ]
+console.log(tinderUser);
 
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
