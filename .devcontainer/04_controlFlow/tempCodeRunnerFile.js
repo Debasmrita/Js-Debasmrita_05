788f23ada81cf7b1201dc6bdb6c6b378b95e0971
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-23 19:45:33 +0000

    commit

--- .devcontainer/04_controlFlow/tempCodeRunnerFile.js
+++ .devcontainer/04_controlFlow/tempCodeRunnerFile.js
@@ -1,10 +1,24 @@
-//if
-// if("2"===2)
-// {
-//     console.log("executed");
+const userEmail =[]
+
+// if (userEmail) {
+//     console.log("GOT user email");
+// } else {
+//     console.log("dont have user emaIL");
 // }
-// else{
-//     console.log("false");
-//     let value= Number("556a");
-//     console.log(value);//NaN    
+
+// //falsy values---
+// // false, 0, -0, BigInt on, "", null, undefined, NaN
+
+// //truthy values---
+// // "0", 'false', " ", [], {}, function(){}, 
+
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+
+// const empty={}
+// if(object.keys(empty).length == 0){
+//     console.log("Empty")
 // }
--- .devcontainer/04_controlFlow/truthy.js
+++ .devcontainer/04_controlFlow/truthy.js
@@ -1,31 +1,43 @@
-const userEmail =[]
+// const userEmail =[]
 
-if (userEmail) {
-    console.log("GOT user email");
-} else {
-    console.log("dont have user emaIL");
-}
+// if (userEmail) {
+//     console.log("GOT user email");
+// } else {
+//     console.log("dont have user emaIL");
+// }
 
-//falsy values---
-// false, 0, -0, BigInt on, "", null, undefined, NaN
+// //falsy values---
+// // false, 0, -0, BigInt on, "", null, undefined, NaN
 
-//truthy values---
-// "0", 'false', " ", [], {}, function(){}, 
+// //truthy values---
+// // "0", 'false', " ", [], {}, function(){}, 
 
 
-if (userEmail.length === 0) {
-    console.log("Array is empty");
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
     
-}
+// }
 
-const empty={}
-if(object.keys(empty).length === 0){
-    console.log("Empty")
-}
+// const empty={}
+// if(object.keys(empty).length == 0){
+//     console.log("Empty")
+// }
 
 
 // nullish coalescing operator (??): null undefined
 
 let val1;
-val1 = 5 ?? 10;
+//val1 = 5 ?? 10;
+//val1= undefined ?? 15
+val1= null ?? 10 ?? 20;
+//val1= null ?? 10;
 console.log(val1)
+
+
+//Ternary Operator
+
+//condition ? true:false
+
+const iceTea= 100;
+iceTea >=80 ? console.log("less than 80") : console.log("more than 80");
+
