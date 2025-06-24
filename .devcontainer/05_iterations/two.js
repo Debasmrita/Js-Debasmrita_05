3838882f733b46d5f978edc1d06cb1579425b5fa
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-06-24 05:26:56 +0000

    commit

---
+++ .devcontainer/05_iterations/two.js
@@ -0,0 +1,21 @@
+// while loop
+
+// let index=0
+// while (index<=10) {
+//     console.log(`value of i is ${index}`);
+//     index= index+2;
+// }
+
+let myarr=["flash", " bataman", "superman"];
+let index=0;
+while (index< myarr.length) {
+    //console.log(`${index} ${myarr[index]}`);
+    index++;
+}
+
+let score=1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<= 20);
