ecaceb300ae6b0e89fbde9ed8b6d4ff5dd07db03
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-07-11 09:53:51 +0000

    prototype

--- .devcontainer/10_OOPs/prototype.js
+++ .devcontainer/10_OOPs/prototype.js
@@ -16,3 +16,48 @@ let heroPower= {
     }
 }
 
+Object.prototype.debu =function(){
+    console.log("Debu is present in all object")
+}
+
+Array.prototype.heyDebu= function(){
+    console.log("HEllo Debu")
+}
+
+myHeros.debu();
+myHeros.heyDebu()
+
+//inheritance
+const User={
+    username: "rai",
+    work: "google"
+}
+const teacher= {
+    makeVideo: true
+}
+
+const TeachingSupport= {
+    isAvailble: false
+}
+
+const TASupport= {
+    DPPAvailble: 'JS Assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+teacher.__proto__=User
+
+//Mordern
+
+Object.setPrototypeOf(TeachingSupport, teacher)
+
+let myName= "Debu     "
+String.prototype.truelength= function(){
+    console.log(myName.truelength)
+    console.log(`${this}`)
+    console.log(`${this.name}`)
+    console.log(`true length is${this.trim().length}`)
+
+}
+ 
+anotherUserName.trueLength()
