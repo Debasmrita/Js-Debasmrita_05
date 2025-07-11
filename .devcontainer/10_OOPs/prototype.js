05001e3c25558158a887e67102d57e2155158e81
Debasmrita <146209834+Debasmrita@users.noreply.github.com>
2025-07-11 07:35:26 +0000

    proto

---
+++ .devcontainer/10_OOPs/prototype.js
@@ -0,0 +1,18 @@
+// let myName= "Debu     "
+
+// console.log(myName.truelength)
+
+
+
+let myHeros=["thor","spiderman"]
+
+let heroPower= {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spiderman power: ${this.spiderman}`);
+        
+    }
+}
+
